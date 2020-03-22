165099c283404c6990e7ec92bde443b65318d135
sigmaboy <j.sigmaboy@gmail.com>
2020-03-22 23:27:32 +0100

    added pyNPU script
    * it can create a human readable changelog
    * it generates download links for latest or all update

---
+++ pyNPU.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+
+# for exiting etc
+import sys
+# used for command line argument parsing
+import argparse
+# for creating the checksum based on hmac key
+import hashlib
+import hmac
+# used to get data from Sony servers
+import requests
+# use lxml for CDATA parsing
+from lxml import etree as xml
+
+# return codes
+# 0 - success
+# 1 - user errors
+# 2 - no update available
+
+# hmac key from http://wololo.net/talk/viewtopic.php?f=54&t=44091
+# thank you Proxima
+key = bytes.fromhex("E5E278AA1EE34082A088279C83F9BBC806821C52F2AB5D2B4ABD995450355114")
+
+program_version = "0.0.1"
+## script parameter handling
+parser = argparse.ArgumentParser(prog='pyNPU', usage='%(prog)s [options]')
+parser.add_argument('--changelog', '-c', action='store_true', help="Print changelog to stdout")
+parser.add_argument('--link', '-l', action='store_true', help="Print download link of the latest update")
+parser.add_argument('--all', '-a', action='store_true', help="Download all updates")
+parser.add_argument('--title', '-t', nargs=1, help="specify the Title ID of game")
+parser.add_argument('--version', '-v', action='version', version='%(prog)s v0.0.1')
+args = parser.parse_args()
+
+if not args.title:
+    print("Error: No Title ID given.")
+    sys.exit(1)
+else:
+    title = args.title[0]
+
+# create the checksum for the url
+hmac_sha256 = hmac.new(key, msg=("np_" + title).encode('utf-8'), digestmod=hashlib.sha256).hexdigest()
+
+# create url from title id and checksum
+update_url = "http://gs-sec.ww.np.dl.playstation.net/pl/np/%s/%s/%s-ver.xml" % (title, hmac_sha256, title)
+
+# get the request
+r = requests.get(update_url, verify=False)
+if r.status_code == 404:
+    print("No update for this game available")
+    sys.exit(2)
+r.close()
+
+# get python object from XML
+update_xml = xml.fromstring(r.content)
+
+# make sure several arguments cannot be combined
+### FIXME better error message ###
+if args.changelog and args.link:
+    print("Cannot combine changelog and link")
+    sys.exit(1)
+elif args.changelog and args.all:
+    print("Cannot combine changelog and all")
+    sys.exit(1)
+
+# not sure if the if the check is really needed
+if update_xml.get('status') == "alive":
+    if args.changelog:
+        for changeinfo in update_xml.iter('changeinfo'):
+            change_url = changeinfo.get('url')
+
+        r = requests.get(change_url)
+        ### create xml parser and make sure CDATA won't be stripped
+        xmlparser = xml.XMLParser(strip_cdata=False)
+        changelog_xml = xml.XML(r.content, xmlparser)
+        r.close()
+
+        for version in changelog_xml:
+            print(version.get('app_ver'))
+            print(version.text.strip())
+    elif args.link:
+        if args.all:
+            for package in update_xml.iter('package'):
+                print(package.get('url'))
+        else:
+            for package in update_xml.iter('package'):
+                if 'version' not in locals():
+                    version = float(package.get('version'))
+                else:
+                    if version < float(package.get('version')):
+                        version = float(package.get('version'))
+
+            for package in update_xml.iter('package'):
+                if float(package.get('version')) == version:
+                    print(package.get('url'))
