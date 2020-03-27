7339a972542f309627d4dd62dfc95853f2c0ef59
sigmaboy <j.sigmaboy@gmail.com>
2020-03-27 17:09:28 +0100

    colors will no be declared once instead of every time in the function

--- download_region.sh
+++ download_region.sh
@@ -33,26 +33,27 @@ check_type() {
         exit 1
     fi
 }
+
+# make color codes human readable
+GREEN="\033[0;32m"
+CYAN="\033[0;36m"
+RED="\033[0;31m"
+PURPLE="\033[0;35m"
+BROWN="\033[0;33m"
+LIGHT_GRAY="\033[0;37m"
+LIGHT_BLUE="\033[1;34m"
+LIGHT_GREEN="\033[1;32m"
+LIGHT_CYAN="\033[1;36m"
+LIGHT_RED="\033[1;31m"
+LIGHT_PURPLE="\033[1;35m"
+YELLOW="\033[1;33m"
+WHITE="\033[1;37m"
+BOLD="\033[1m"
+RESET="\033[0m" #0m restores to the terminal's default colour
+
 check_return_code() {
     local NUMBER="${1}"
     local TYPE="${2}"
-
-    # make color codes human readable
-    local GREEN="\033[0;32m"
-    local CYAN="\033[0;36m"
-    local RED="\033[0;31m"
-    local PURPLE="\033[0;35m"
-    local BROWN="\033[0;33m"
-    local LIGHT_GRAY="\033[0;37m"
-    local LIGHT_BLUE="\033[1;34m"
-    local LIGHT_GREEN="\033[1;32m"
-    local LIGHT_CYAN="\033[1;36m"
-    local LIGHT_RED="\033[1;31m"
-    local LIGHT_PURPLE="\033[1;35m"
-    local YELLOW="\033[1;33m"
-    local WHITE="\033[1;37m"
-    local BOLD="\033[1m"
-    local RESET="\033[0m" #0m restores to the terminal's default colour
 
     case ${NUMBER} in
         2)
