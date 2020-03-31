53543656dde9128e10605371bd4b0a68b5db3920
sigmaboy <j.sigmaboy@gmail.com>
2020-03-31 19:51:12 +0200

    directory now gets its name from files and from pyNPU.py

--- download_update.sh
+++ download_update.sh
@@ -57,6 +57,8 @@ then
     exit 2
 fi
 
+GAME_NAME="$(pyNPU.py --name --title-id "${TITLE_ID}")"
+
 # make DESTDIR overridable
 if [ -z "${DESTDIR}" ]
 then
@@ -112,3 +114,5 @@ do
         cd "${MY_PATH}"
     fi
 done
+REGION_NAME="$(basename "$(find "${MY_PATH}/${DESTDIR}_update" -type f -name "*.${ext}" | head -n 1)" | sed "s/.*\[${TITLE_ID}\] \[//g" | sed 's/\] \[PATCH.*//')"
+mv "${MY_PATH}/${DESTDIR}_update" "${MY_PATH}/${GAME_NAME} [${TITLE_ID}] [${REGION_NAME}]_update"
