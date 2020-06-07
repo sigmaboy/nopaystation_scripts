0fa9b891eff8112c9b06223c2a9d44b550b87568
sigmaboy <j.sigmaboy@gmail.com>
2020-06-07 17:22:13 +0200

    nps_psm: removed unneeded t7z dependency

--- nps_psm.sh
+++ nps_psm.sh
@@ -20,7 +20,7 @@ my_usage() {
     echo "${0} \"/path/to/PSM.tsv\" \"PCSE00986\""
 }
 
-MY_BINARIES="pkg2zip sed grep file t7z"
+MY_BINARIES="pkg2zip sed grep file"
 sha256_choose; downloader_choose
 
 check_binaries "${MY_BINARIES}"
